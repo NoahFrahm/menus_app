cdabda4c65ecf5c05a410655fc7d66ad614cb7c0
noahfrahm <nofrahm@live.unc.edu>
2021-10-12 16:21:56 -0400

    updated pomxml jsoup dependecny to 1.14.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         public static void main(String[] args) throws IOException {
             GetMenus menus = new GetMenus("top-of-lenoir");
 //        GetHours todays_hours = new GetHours();
+//       add code to create boolean hasmap of favorited items that are in the dining hall for each day
     }
 }
